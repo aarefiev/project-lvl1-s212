a0f75d90d8042696d24582423a839e7bf11b0bda
Artem Arefiev <a.arefiev@inbox.ru>
2017-12-12 18:16:06 +0400

    refactor index

--- src/index.js
+++ src/index.js
@@ -1,16 +1,49 @@
 import readlineSync from 'readline-sync';
 
-const getUserAnswer = answer => readlineSync.question(answer, '');
+// UI functions
+const WELCOME_MESSAGE = 'Welcome to the Brain Games!';
+
+const printMessage = message => console.log(message);
+const printHello = userName => printMessage(`Hello, ${userName}!\n`);
+const printQuestion = details => printMessage(`Question: ${details}`);
+const printRight = () => printMessage('Correct!');
+const printWrong = (answer, rightAnswer, userName) => {
+  const message = `'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'. \nLet's try again, ${userName}!`;
+
+  return printMessage(message);
+};
+const printCongrat = userName => printMessage(`Congratulations, ${userName}!`);
+
+const getUserAnswer = answer => readlineSync.question(answer);
 const getUserName = () => getUserAnswer('May I have your name? ');
+const getGameUserAnswer = (question = '') => {
+  printQuestion(question);
+  return getUserAnswer('Your answer: ');
+};
 
+// Math functions
 const getRandomNumber = () => Math.floor(Math.random() * 10) + 1;
-const isEvenNumber = num => Number.isInteger(num) && num % 2 === 0;
-const getIsEvenNumberAnswer = num => (isEvenNumber(num) ? 'yes' : 'no');
-const getIsEvenNumberUserAnswer = num => getUserAnswer(`Question: ${num}\nYour answer: `);
+const getRandomOperation = (ops = ['add', 'div', 'multiply']) => {
+  if (!(ops instanceof Array) && ops.length === 0) {
+    return false;
+  }
 
-export default getUserName;
-export {
+  return ops[Math.floor(Math.random() * ops.length)];
+};
+
+export const ui = {
+  WELCOME_MESSAGE,
+  printMessage,
+  printHello,
+  printQuestion,
+  printRight,
+  printWrong,
+  printCongrat,
+  getUserAnswer,
+  getUserName,
+  getGameUserAnswer,
+};
+export const math = {
   getRandomNumber,
-  isEvenNumber,
-  getIsEvenNumberAnswer,
-  getIsEvenNumberUserAnswer };
+  getRandomOperation,
+};
