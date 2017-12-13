9c778b86b20c112e8a7bbbfc31464ff613756adb
Artem Arefiev <a.arefiev@inbox.ru>
2017-12-13 11:34:00 +0400

    add logic without question to game

--- src/index.js
+++ src/index.js
@@ -39,40 +39,45 @@ const config = {
   name: 'game',
   task: '',
   attemptsNumber: 3,
-  getQuestion: (x) => x,
-  getAnswer: (x) => x,
+  getQuestion: null,
+  getAnswer: null,
 };
 
 // Game
 const game = () => {
   printMessage('\nWelcome to the Brain Games!');
-  printMessage(`${config.task}`);
+
+  if (config.task.length !== 0) {
+    printMessage(`${config.task}`);
+  }
 
   const userName = getUserAnswer('\nMay I have your name? ');
 
   printMessage(`Hello, ${userName}!\n`);
 
-  let attemptNumber = 0;
+  if (config.getQuestion !== null) {
+    let attemptNumber = 0;
 
-  while (attemptNumber < config.attemptsNumber) {
-    const question = config.getQuestion();
-    const answer = config.getAnswer(question);
+    while (attemptNumber < config.attemptsNumber) {
+      const question = config.getQuestion();
+      const answer = config.getAnswer(question);
 
-    printMessage(`Question: ${question}`);
+      printMessage(`Question: ${question}`);
 
-    const userAnswer = getUserAnswer('Your answer: ');
+      const userAnswer = getUserAnswer('Your answer: ');
 
-    if (userAnswer !== answer) {
-      printMessage(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'. \nLet's try again, ${userName}!`);
-      break;
+      if (userAnswer !== answer) {
+        printMessage(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'. \nLet's try again, ${userName}!`);
+        break;
+      }
+
+      printMessage('Correct!');
+      attemptNumber += 1;
     }
 
-    printMessage('Correct!');
-    attemptNumber += 1;
-  }
-
-  if (attemptNumber === config.attemptsNumber) {
-    printMessage(`Congratulations, ${userName}!`);
+    if (attemptNumber === config.attemptsNumber) {
+      printMessage(`Congratulations, ${userName}!`);
+    }
   }
 
   return;
