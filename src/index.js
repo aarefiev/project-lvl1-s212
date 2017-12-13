e58db794634556c67221e9fbdb4fa1ff4adb48f6
Artem Arefiev <a.arefiev@inbox.ru>
2017-12-13 08:39:50 +0400

    add interface

--- src/index.js
+++ src/index.js
@@ -1,49 +1,78 @@
 import readlineSync from 'readline-sync';
 
-// UI functions
-const WELCOME_MESSAGE = 'Welcome to the Brain Games!';
+// // UI functions
+// const WELCOME_MESSAGE = 'Welcome to the Brain Games!';
+//
+// const printMessage = message => console.log(message);
+// const printHello = userName => printMessage(`Hello, ${userName}!\n`);
+// const printQuestion = details => printMessage(`Question: ${details}`);
+// const printRight = () => printMessage('Correct!');
+// const printWrong = (answer, rightAnswer, userName) => {
+//   const message = `'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'. \nLet's try again, ${userName}!`;
+//
+//   return printMessage(message);
+// };
+// const printCongrat = userName => printMessage(`Congratulations, ${userName}!`);
+//
+// const getUserAnswer = answer => readlineSync.question(answer);
+// const getUserName = () => getUserAnswer('May I have your name? ');
+// const getGameUserAnswer = (question = '') => {
+//   printQuestion(question);
+//   return getUserAnswer('Your answer: ');
+// };
+//
+// // Math functions
+// const getRandomNumber = () => Math.floor(Math.random() * 10) + 1;
+// const getRandomOperation = (ops = ['add', 'div', 'multiply']) => {
+//   if (!(ops instanceof Array) && ops.length === 0) {
+//     return false;
+//   }
+//
+//   return ops[Math.floor(Math.random() * ops.length)];
+// };
 
-const printMessage = message => console.log(message);
-const printHello = userName => printMessage(`Hello, ${userName}!\n`);
-const printQuestion = details => printMessage(`Question: ${details}`);
-const printRight = () => printMessage('Correct!');
-const printWrong = (answer, rightAnswer, userName) => {
-  const message = `'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'. \nLet's try again, ${userName}!`;
-
-  return printMessage(message);
-};
-const printCongrat = userName => printMessage(`Congratulations, ${userName}!`);
 
 const getUserAnswer = answer => readlineSync.question(answer);
-const getUserName = () => getUserAnswer('May I have your name? ');
-const getGameUserAnswer = (question = '') => {
-  printQuestion(question);
-  return getUserAnswer('Your answer: ');
+const printMessage = message => console.log(message);
+
+const config = {
+  name: 'game',
+  task: '',
+  attemptsNumber: 3,
+  getQuestion: (x) => x,
+  getAnswer: (x) => x,
 };
 
-// Math functions
-const getRandomNumber = () => Math.floor(Math.random() * 10) + 1;
-const getRandomOperation = (ops = ['add', 'div', 'multiply']) => {
-  if (!(ops instanceof Array) && ops.length === 0) {
-    return false;
+// Game
+const game = () => {
+  printMessage('\nWelcome to the Brain Games!');
+  printMessage(`${config.task}`);
+
+  const userName = getUserAnswer('\nMay I have your name? ');
+
+  printMessage(`Hello, ${userName}!\n`);
+
+  let attemptNumber = 1;
+
+  while (attemptNumber <= config.attemptsNumber) {
+    const question = config.getQuestion();
+    const answer = config.getAnswer(question);
+
+    printMessage(`Question: ${question}`);
+
+    const userAnswer = getUserAnswer('Your answer: ');
+
+    if (userAnswer !== answer) {
+      printMessage(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'. \nLet's try again, ${userName}!`);
+      break;
+    }
+
+    printMessage('Correct!');
+    attemptNumber += 1;
   }
 
-  return ops[Math.floor(Math.random() * ops.length)];
-};
+  printMessage(`Congratulations, ${userName}!`);
+  return;
+}
 
-export const ui = {
-  WELCOME_MESSAGE,
-  printMessage,
-  printHello,
-  printQuestion,
-  printRight,
-  printWrong,
-  printCongrat,
-  getUserAnswer,
-  getUserName,
-  getGameUserAnswer,
-};
-export const math = {
-  getRandomNumber,
-  getRandomOperation,
-};
+export { config, game };
