628a8dc5a2f5fbe97b203c28610a4bcf8fd43582
Artem Arefiev <a.arefiev@inbox.ru>
2017-12-13 11:20:11 +0400

    refactor brain-calc task

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,79 +1,68 @@
-import { ui, math } from '..';
+import { config, game } from '..';
+
+const getRandomNumber = () => Math.floor(Math.random() * 10) + 1;
+const getRandomOperation = (ops = ['add', 'div', 'multiply']) => {
+  if (!(ops instanceof Array) && ops.length === 0) {
+    return false;
+  }
+
+  return ops[Math.floor(Math.random() * ops.length)];
+};
 
 const generateEquation = () => {
-  const equation = {
-    numbers: [math.getRandomNumber(), math.getRandomNumber()],
-    operation: math.getRandomOperation(),
+  function Equation() {
+    this.numbers = [getRandomNumber(), getRandomNumber()];
+    this.operation = getRandomOperation();
+
+    return this;
+  };
+  Equation.prototype.toString = function() {
+    const operationsAlphabet = {
+      add: '+',
+      div: '-',
+      multiply: '*',
+    };
+    const operationSign = operationsAlphabet[this.operation];
+    const iter = (numbers, acc) => {
+      const current = numbers[acc];
+      const newAcc = acc + 1;
+
+      if (typeof numbers[newAcc] === 'undefined') {
+        return `${current}`;
+      }
+
+      return `${current} ${operationSign} ${iter(numbers, newAcc)}`;
+    };
+
+    return iter(this.numbers, 0);
+  };
+  Equation.prototype.calculate = function() {
+    const iter = (numbers, acc) => {
+      const current = numbers[acc];
+      const newAcc = acc + 1;
+
+      if (typeof numbers[newAcc] === 'undefined') {
+        return current;
+      }
+
+      if (this.operation === 'add') {
+        return current + iter(numbers, newAcc);
+      } else if (this.operation === 'div') {
+        return current - iter(numbers, newAcc);
+      }
+
+      return current * iter(numbers, newAcc);
+    };
+
+    return iter(this.numbers, 0);
   };
 
-  return equation;
-};
-const equationToString = (eq) => {
-  const operationsAlphabet = {
-    add: '+',
-    div: '-',
-    multiply: '*',
-  };
-  const operationSign = operationsAlphabet[eq.operation];
-  const iter = (numbers, acc) => {
-    const current = numbers[acc];
-    const newAcc = acc + 1;
-
-    if (typeof numbers[newAcc] === 'undefined') {
-      return `${current}`;
-    }
-
-    return `${current} ${operationSign} ${iter(numbers, newAcc)}`;
-  };
-
-  return iter(eq.numbers, 0);
-};
-const calcEquation = (eq) => {
-  const iter = (numbers, acc) => {
-    const current = numbers[acc];
-    const newAcc = acc + 1;
-
-    if (typeof numbers[newAcc] === 'undefined') {
-      return current;
-    }
-
-    if (eq.operation === 'add') {
-      return current + iter(numbers, newAcc);
-    } else if (eq.operation === 'div') {
-      return current - iter(numbers, newAcc);
-    }
-
-    return current * iter(numbers, newAcc);
-  };
-
-  return iter(eq.numbers, 0);
+  return new Equation();
 };
 
-const game = () => {
-  ui.printMessage(`${ui.WELCOME_MESSAGE}\nWhat is the result of the expression?\n`);
-
-  const userName = ui.getUserName();
-  let questionsCount = 3;
-
-  ui.printHello(userName);
-
-  while (questionsCount > 0) {
-    const eq = generateEquation();
-    const answer = String(calcEquation(eq));
-    const userAnswer = ui.getGameUserAnswer(equationToString(eq));
-
-    if (answer !== userAnswer) {
-      ui.printWrong(userAnswer, answer, userName);
-      break;
-    }
-
-    ui.printRight();
-    questionsCount -= 1;
-  }
-
-  if (questionsCount === 0) {
-    ui.printCongrat(userName);
-  }
-};
+config.name = 'brain-calc';
+config.task = 'What is the result of the expression?';
+config.getQuestion = () => generateEquation();
+config.getAnswer = (question) => String(question.calculate());
 
 export default game;
