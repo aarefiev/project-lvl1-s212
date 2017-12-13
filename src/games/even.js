05cd4198321222e18471e117a30cf25209ba02fb
Artem Arefiev <a.arefiev@inbox.ru>
2017-12-13 18:19:00 +0400

    refactor brain-even to generate question through QuestionGenerator

--- src/games/even.js
+++ src/games/even.js
@@ -1,11 +1,17 @@
 import { config, game } from '..';
+import QuestionGenerator from '../question_generator/question_generator';
 
-const isEvenNumber = num => ((Number.isInteger(num) && num % 2 === 0) ? 'yes' : 'no');
 const getRandomNumber = () => Math.floor(Math.random() * 100) + 1;
+const isEvenNumber = num => Number.isInteger(num) && num % 2 === 0;
+const getCorrectAnswer = num => (isEvenNumber(num) ? 'yes' : 'no');
 
 config.name = 'brain-even';
 config.task = 'Answer "yes" if number even otherwise answer "no".';
-config.getQuestion = () => getRandomNumber();
-config.getAnswer = question => isEvenNumber(question);
+config.questionGenerator = () => {
+  const question = getRandomNumber();
+  const answer = getCorrectAnswer(question);
+
+  return new QuestionGenerator(question, answer);
+};
 
 export default game;
