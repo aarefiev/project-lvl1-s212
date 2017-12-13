035ad97f4fc9e405e00369a1bf1dba31e592a1aa
Artem Arefiev <a.arefiev@inbox.ru>
2017-12-13 11:28:32 +0400

    refactor brain-gcd task

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,47 +1,51 @@
-import { ui, math } from '..';
+import { config, game } from '..';
 
-const calculateGCD = (num1, num2) => {
-  const iter = (n1, n2) => {
-    if (n2 === 0) {
-      return n1;
+const getRandomNumber = () => Math.floor(Math.random() * 100) + 1;
+const generateNumbers = () => {
+  function Numbers() {
+    const num1 = getRandomNumber();
+    const num2 = getRandomNumber();
+
+    if (num1 > num2) {
+      this.numbers = [num2, num1];
+    } else {
+      this.numbers = [num1, num2];
     }
 
-    return iter(n2, n1 % n2);
+    return this;
+  };
+  Numbers.prototype.toString = function() {
+    const iter = (numbers, acc) => {
+      const current = numbers[acc];
+      const newAcc = acc + 1;
+
+      if (typeof numbers[newAcc] === 'undefined') {
+        return `${current}`;
+      }
+
+      return `${current} ${iter(numbers, newAcc)}`;
+    };
+
+    return iter(this.numbers, 0);
+  };
+  Numbers.prototype.calculate = function() {
+    const iter = (n1, n2) => {
+      if (n2 === 0) {
+        return n1;
+      }
+
+      return iter(n2, n1 % n2);
+    };
+
+    return iter(this.numbers[0], this.numbers[1]);
   };
 
-  if (num1 > num2) {
-    return iter(num2, num1);
-  }
-
-  return iter(num1, num2);
+  return new Numbers();
 };
 
-const game = () => {
-  ui.printMessage(`${ui.WELCOME_MESSAGE}\nFind the greatest common divisor of given numbers.\n`);
-
-  const userName = ui.getUserName();
-  let questionsCount = 3;
-
-  ui.printHello(userName);
-
-  while (questionsCount > 0) {
-    const num1 = math.getRandomNumber();
-    const num2 = math.getRandomNumber();
-    const answer = String(calculateGCD(num1, num2));
-    const userAnswer = ui.getGameUserAnswer(`${num1} ${num2}`);
-
-    if (answer !== userAnswer) {
-      ui.printWrong(userAnswer, answer, userName);
-      break;
-    }
-
-    ui.printRight();
-    questionsCount -= 1;
-  }
-
-  if (questionsCount === 0) {
-    ui.printCongrat(userName);
-  }
-};
+config.name = 'brain-gcd';
+config.task = 'Find the greatest common divisor of given numbers.';
+config.getQuestion = () => generateNumbers();
+config.getAnswer = (question) => String(question.calculate());
 
 export default game;
