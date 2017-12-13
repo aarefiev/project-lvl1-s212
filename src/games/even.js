7bd85ef113bb2808c158087198cca1bdf201c915
Artem Arefiev <a.arefiev@inbox.ru>
2017-12-13 21:35:52 +0400

    refactor brain-even

--- src/games/even.js
+++ src/games/even.js
@@ -1,17 +1,21 @@
-import { config, game } from '..';
+import { game } from '..';
 import QuestionGenerator from '../question_generator/question_generator';
 
 const getRandomNumber = () => Math.floor(Math.random() * 100) + 1;
 const isEvenNumber = num => Number.isInteger(num) && num % 2 === 0;
 const getCorrectAnswer = num => (isEvenNumber(num) ? 'yes' : 'no');
 
-config.name = 'brain-even';
-config.task = 'Answer "yes" if number even otherwise answer "no".';
-config.questionGenerator = () => {
-  const question = getRandomNumber();
-  const answer = getCorrectAnswer(question);
+// evenGame
+const evenGame = () => {
+  const task = 'Answer "yes" if number even otherwise answer "no".';
+  const questionGenerator = () => {
+    const question = getRandomNumber();
+    const answer = getCorrectAnswer(question);
 
-  return new QuestionGenerator(question, answer);
+    return new QuestionGenerator(question, answer);
+  };
+
+  return game(task, questionGenerator);
 };
 
-export default game;
+export default evenGame;
