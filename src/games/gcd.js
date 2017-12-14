02b480998c57318d0170a175b864a752cb619bb7
Artem Arefiev <a.arefiev@inbox.ru>
2017-12-14 20:09:39 +0400

    remove bad functions

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -2,7 +2,6 @@ import { game } from '..';
 import QuestionGenerator from '../question_generator/question_generator';
 
 const getRandomNumber = () => Math.floor(Math.random() * 100) + 1;
-const numbersToString = numbers => numbers.join(' ');
 const calculateGCD = (num1, num2) => {
   if (num2 === 0) {
     return num1;
@@ -10,15 +9,15 @@ const calculateGCD = (num1, num2) => {
 
   return calculateGCD(num2, num1 % num2);
 };
-const numbersGenerator = () => [getRandomNumber(), getRandomNumber()];
 
 // gcdGame
 const gcdGame = () => {
   const task = 'Find the greatest common divisor of given numbers.';
   const questionGenerator = () => {
-    const numbers = numbersGenerator();
-    const question = numbersToString(numbers);
-    const answer = String(calculateGCD(numbers[0], numbers[1]));
+    const num1 = getRandomNumber();
+    const num2 = getRandomNumber();
+    const question = `${num1} ${num2}`;
+    const answer = String(calculateGCD(num1, num2));
 
     return new QuestionGenerator(question, answer);
   };
